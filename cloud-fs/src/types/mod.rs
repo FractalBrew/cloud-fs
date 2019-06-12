e65ec7bddfec8ac682515efdd435a837fe323aa9
Dave Townsend <dtownsend@oxymoronical.com>
2019-06-11 21:26:25 -0700

    Break FsPath out into its own module.

---
+++ cloud-fs/src/types/mod.rs
@@ -0,0 +1,105 @@
+mod path;
+
+use std::cmp::{Ord, Ordering};
+use std::error::Error;
+use std::fmt;
+use std::io;
+
+use bytes::Bytes;
+
+use crate::backends::Backend;
+pub use path::FsPath;
+
+pub type Data = Bytes;
+
+#[derive(Clone, Debug)]
+pub enum FsErrorType {
+    ParseError,
+    HostNotSupported,
+    InvalidPath,
+    Other,
+}
+
+#[derive(Clone, Debug)]
+pub struct FsError {
+    error_type: FsErrorType,
+    description: String,
+}
+
+impl FsError {
+    pub(crate) fn new<S: AsRef<str>>(error_type: FsErrorType, description: S) -> FsError {
+        FsError {
+            error_type,
+            description: description.as_ref().to_owned(),
+        }
+    }
+
+    pub(crate) fn from_error<E>(error: E) -> FsError
+    where
+        E: Error + fmt::Display,
+    {
+        Self::new(FsErrorType::Other, format!("{}", error))
+    }
+}
+
+impl fmt::Display for FsError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        fmt.write_str(&self.description)
+    }
+}
+
+impl Error for FsError {}
+
+impl From<io::Error> for FsError {
+    fn from(e: io::Error) -> FsError {
+        FsError::from_error(e)
+    }
+}
+
+pub type FsResult<R> = Result<R, FsError>;
+
+#[derive(Clone, Debug)]
+pub struct FsSettings {
+    pub(crate) backend: Backend,
+    pub(crate) path: FsPath,
+}
+
+impl FsSettings {
+    pub fn new(backend: Backend, path: FsPath) -> FsSettings {
+        FsSettings { backend, path }
+    }
+
+    pub fn backend(&self) -> &Backend {
+        &self.backend
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct FsFile {
+    pub(crate) path: FsPath,
+    pub(crate) size: Option<u64>,
+}
+
+impl FsFile {
+    pub fn path(&self) -> &FsPath {
+        &self.path
+    }
+
+    pub fn size(&self) -> Option<u64> {
+        self.size
+    }
+}
+
+impl Eq for FsFile {}
+
+impl PartialOrd for FsFile {
+    fn partial_cmp(&self, other: &FsFile) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for FsFile {
+    fn cmp(&self, other: &FsFile) -> Ordering {
+        self.path.cmp(&other.path)
+    }
+}
--- cloud-fs/src/types/path.rs
+++ cloud-fs/src/types/path.rs
@@ -1,61 +1,9 @@
 use std::cmp::min;
 use std::cmp::{Ord, Ordering};
-use std::error::Error;
 use std::fmt;
-use std::io;
 use std::path::Path;
 
-use bytes::Bytes;
-
-use crate::backends::Backend;
-
-pub type Data = Bytes;
-
-#[derive(Clone, Debug)]
-pub enum FsErrorType {
-    ParseError,
-    HostNotSupported,
-    InvalidPath,
-    Other,
-}
-
-#[derive(Clone, Debug)]
-pub struct FsError {
-    error_type: FsErrorType,
-    description: String,
-}
-
-impl FsError {
-    pub(crate) fn new<S: AsRef<str>>(error_type: FsErrorType, description: S) -> FsError {
-        FsError {
-            error_type,
-            description: description.as_ref().to_owned(),
-        }
-    }
-
-    pub(crate) fn from_error<E>(error: E) -> FsError
-    where
-        E: Error + fmt::Display,
-    {
-        Self::new(FsErrorType::Other, format!("{}", error))
-    }
-}
-
-impl fmt::Display for FsError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.write_str(&self.description)
-    }
-}
-
-impl Error for FsError {}
-
-impl From<io::Error> for FsError {
-    fn from(e: io::Error) -> FsError {
-        FsError::from_error(e)
-    }
-}
-
-pub type FsResult<R> = Result<R, FsError>;
+use crate::types::{FsError, FsErrorType, FsResult};
 
 const PARENT_DIR: &str = "..";
 const CURRENT_DIR: &str = ".";
@@ -479,52 +427,6 @@ impl Ord for FsPath {
 impl AsRef<FsPath> for FsPath {
     fn as_ref(&self) -> &FsPath {
         self
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct FsSettings {
-    pub(crate) backend: Backend,
-    pub(crate) path: FsPath,
-}
-
-impl FsSettings {
-    pub fn new(backend: Backend, path: FsPath) -> FsSettings {
-        FsSettings { backend, path }
-    }
-
-    pub fn backend(&self) -> &Backend {
-        &self.backend
-    }
-}
-
-#[derive(Clone, PartialEq, Debug)]
-pub struct FsFile {
-    pub(crate) path: FsPath,
-    pub(crate) size: Option<u64>,
-}
-
-impl FsFile {
-    pub fn path(&self) -> &FsPath {
-        &self.path
-    }
-
-    pub fn size(&self) -> Option<u64> {
-        self.size
-    }
-}
-
-impl Eq for FsFile {}
-
-impl PartialOrd for FsFile {
-    fn partial_cmp(&self, other: &FsFile) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for FsFile {
-    fn cmp(&self, other: &FsFile) -> Ordering {
-        self.path.cmp(&other.path)
     }
 }
 
